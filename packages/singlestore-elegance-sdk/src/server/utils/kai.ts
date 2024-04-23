8f6e0a7bf5d383a4c62ef0a37dbbb10de5cb620c
Yaroslav Demenskyi <ydemenskyi@singlestore.com>
2024-04-23 10:49:17 -0700

    v0.0.21

--- packages/singlestore-elegance-sdk/src/server/utils/kai.ts
+++ packages/singlestore-elegance-sdk/src/server/utils/kai.ts
@@ -1,16 +1,14 @@
 import { MongoClient } from "mongodb";
 import type { KaiConnection, KaiConnectionConfig } from "../../shared/types";
 
-export function createKaiConnection(config: KaiConnectionConfig): KaiConnection {
-  if (!config.uri) {
-    throw new Error("Kai URI is undefined");
-  }
-
+export function createKaiConnection(
+  config: KaiConnectionConfig
+): KaiConnection {
   const { uri, database, ...options } = config;
 
   const client = new MongoClient(uri, options);
 
-  const db: KaiConnection["db"] = dbName => {
+  const db: KaiConnection["db"] = (dbName) => {
     return client.db(dbName || database || "");
   };
 
@@ -18,6 +16,6 @@ export function createKaiConnection(config: KaiConnectionConfig): KaiConnection 
     type: "kai",
     dbName: database,
     client,
-    db
+    db,
   };
 }
--- packages/singlestore-elegance-sdk/src/server/utils/mysql.ts
+++ packages/singlestore-elegance-sdk/src/server/utils/mysql.ts
@@ -1,24 +1,19 @@
 import { createPool } from "mysql2";
-import type { MySQLConnectionConfig, MySQLConnection } from "../../shared/types";
+import type {
+  MySQLConnectionConfig,
+  MySQLConnection,
+} from "../../shared/types";
 
-export function createMySQLConnection(config: MySQLConnectionConfig): MySQLConnection {
+export function createMySQLConnection(
+  config: MySQLConnectionConfig
+): MySQLConnection {
   const { database, ..._config } = config;
-
-  if (!_config.host) {
-    throw new Error("Host is undefined");
-  }
-
-  if (!_config.user) {
-    throw new Error("User is undefined");
-  }
-
-  if (!_config.password) {
-    throw new Error("Password is undefined");
-  }
 
   const tablePath: MySQLConnection["tablePath"] = (table, dbName) => {
     const _dbName = dbName || database || "";
-    return typeof _dbName === "string" && _dbName.length ? `${_dbName}.${table}` : table;
+    return typeof _dbName === "string" && _dbName.length
+      ? `${_dbName}.${table}`
+      : table;
   };
 
   const pool = createPool({ ..._config, database }).promise();
@@ -26,11 +21,14 @@ export function createMySQLConnection(config: MySQLConnectionConfig): MySQLConne
   return Object.assign(pool, {
     type: "mysql",
     dbName: database,
-    tablePath
+    tablePath,
   } satisfies Pick<MySQLConnection, "type" | "dbName" | "tablePath">);
 }
 
-export function toInsertValuesQuery(tablePath: string, value: object | object[]) {
+export function toInsertValuesQuery(
+  tablePath: string,
+  value: object | object[]
+) {
   const values = Array.isArray(value) ? value : [value];
   const keys = Object.keys(values[0]);
   const query = `INSERT INTO ${tablePath} (${keys.join(", ")}) VALUES ?`;
